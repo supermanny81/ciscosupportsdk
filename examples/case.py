b7bf3e7666478b60a55c2ace555c305da69d40b0
Manny Garcia <mannygar@cisco.com>
2022-03-03 17:36:37 -0800

    formatting changes

--- examples/case.py
+++ examples/case.py
@@ -2,18 +2,15 @@ import os
 
 from ciscosupportsdk.api import CiscoSupportAPI
 
-CS_API_KEY = os.getenv('CS_API_KEY')
-CS_API_SECRET = os.getenv('CS_API_SECRET')
+CS_API_KEY = os.getenv("CS_API_KEY")
+CS_API_SECRET = os.getenv("CS_API_SECRET")
 
 cs = CiscoSupportAPI(CS_API_KEY, CS_API_SECRET)
 
-print(cs.case.get_case_details('692277140'))
+print(cs.case.get_case_details("692277140"))
 
-for case in cs.case.get_cases_by_contract_id(['92511831']):
-    print(f'{case.case_id}')
+for case in cs.case.get_cases_by_contract_id(["92511831"]):
+    print(f"{case.case_id}")
 
-for case in cs.case.get_cases_by_user_id(['whouse', 'anjs']):
+for case in cs.case.get_cases_by_user_id(["whouse", "anjs"]):
     print(case)
-
-
-# CSCvc57217
